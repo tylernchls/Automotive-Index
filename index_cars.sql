4c7a5528c09c26de3ce9885b3a37f6087ed2c972
Tyler Nichols <tylernchls@gmail.com>
2017-01-13 11:59:36 -1000

    ran query for LAM to ingnore duplicates

---
+++ index_cars.sql
@@ -0,0 +1,7 @@
+-- Timing 1   Time: 71.622 ms
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+
+
+
+
+
--- indexing.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE USER indexed_cars_user;
-
-CREATE DATABASE index_cars;
-
-ALTER DATABASE index_cars OWNER TO indexed_cars_user;
-
