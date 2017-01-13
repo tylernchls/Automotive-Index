756672617b6cbb11fb572f4a01f61ea36690169d
Tyler Nichols <tylernchls@gmail.com>
2017-01-13 13:54:50 -1000

    recored all times for queries ran with index on table

--- index_cars.sql
+++ index_cars.sql
@@ -1,9 +1,30 @@
-
+-- creates index on all model_titles & make_code
 CREATE INDEX car_titles_code
   ON car_models (model_title, make_code);
 
+-- creates index on all make_titles
 CREATE INDEX car_make_titles
   ON car_models (make_title);
+
+-- Time w/index 69.169 ms
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+
+-- Time w/index 66.289 ms
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND  model_code = 'GT-R';
+
+-- looks at all models and grabs by 'lam' then counts all the rows
+-- Time w/index 68.910 ms
+SELECT count(*)  FROM car_models WHERE make_code = 'LAM';
+
+-- searches and counts cars from year 2010 to 2015
+-- Time w/index 99.216 ms
+SELECT count(*) FROM car_models WHERE year BETWEEN 2010 AND 2015;
+
+-- searches all cars with year 2010
+-- Time w/index 81.286 ms
+SELECT count(*) FROM car_models WHERE year = '2010';
+
+
 
 
 
--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -11,22 +11,19 @@ CREATE TABLE IF NOT EXISTS car_models
  year integer NOT NULL,
  PRIMARY KEY (id)
 )
--- Time: 71.622 ms  Time w/index 69.169 ms
+-- Time: 71.622 ms
 SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
 
--- Time: 117.308 ms    Time w/index 66.289 ms
+-- Time: 117.308 ms
 SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND  model_code = 'GT-R';
 
 -- Time: 70.396 ms  looks at all models and grabs by 'lam' then counts all the rows
--- Time w/index 68.910 ms
 SELECT count(*)  FROM car_models WHERE make_code = 'LAM';
 
 -- Time: 132.636 ms searches and counts cars from year 2010 to 2015
--- Time w/index 99.216 ms
 SELECT count(*) FROM car_models WHERE year BETWEEN 2010 AND 2015;
 
 -- Time: 81.562 ms  searches all cars with year 2010
--- Time w/index 81.286 ms
 SELECT count(*) FROM car_models WHERE year = '2010';
 
 
